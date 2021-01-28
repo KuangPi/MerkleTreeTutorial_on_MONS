a249c67f686413162ca059c7bdd6f510320e23a3
KuangPi <927351390@qq.com>
2021-01-28 09:22:07 +0800

    Block Header updates. Could become a chain now.

--- Processer.py
+++ Processer.py
@@ -10,16 +10,17 @@ import datetime
 
 
 class Block:
-    def __init__(self, user1, user2, money):
+    def __init__(self, user1, user2, money, previous=None):
         """
         The unit of a block chain.
         :param user1:
         :param user2:
         :param money:
+        :param previous:
         """
         # todo Init header and body based on information give in one go.
         self.block_body = BlockBody(user1, user2, money)
-        self.block_header = BlockHeader(self.block_body)
+        self.block_header = BlockHeader(self.block_body, previous)
 
     def __str__(self):
         return f"\nHeader: \n{self.block_header}\nBody: \n{self.block_body}"
@@ -56,18 +57,25 @@ class BlockHeader:
             self.previous_block = None
         else:
             # todo Add the case where is not the first block
-            pass
-
+            self.number_of_transactions = previous_block.block_header.number_of_transactions + 1
+            # todo Change the hash root to merkle root
+            self.merkle_root = sha256(block_body.__repr__())
+            self.previous_block = previous_block
+        # Time stamp seems to be a bit useless since it only reflects the current time.
         self.time_stamp = str(datetime.datetime.today())
-        self.hash_root = sha256(block_body.__repr__())
+        self.hash_root = sha256(block_body.__repr__() + self.__repr__())  # This is dangerous to call repr in init.
 
     def __str__(self):
-        temp = f"number_of_transactions: {self.number_of_transactions}\n" \
+        return f"number_of_transactions: {self.number_of_transactions}\n" \
                f"time_stamp: {self.time_stamp}\n" \
                f"merkle_root: {self.merkle_root}\n" \
                f"hash_root: {self.hash_root}"
 
-        return temp
+    def __repr__(self):
+        return f"number_of_transactions: {self.number_of_transactions}\n" \
+               f"time_stamp: {self.time_stamp}\n" \
+               f"merkle_root: {self.merkle_root}\n" \
+
 
 
 class MerkleTree:
@@ -120,7 +128,10 @@ def create_block_chain_accordingg_to_txt(file_name):
         if len(element) != 3:
             raise ValueError("Number of information required to build block chain is not enough! 3 each! ")
         else:
-            result.append(Block(element[0], element[1], int(element[2])))
+            if len(result) == 0:
+                result.append(Block(element[0], element[1], int(element[2])))
+            else:
+                result.append(Block(element[0], element[1], int(element[2]), result[-1]))
     return result
 
 
